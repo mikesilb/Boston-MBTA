b7c1c8c4a81e1501e8fd87d31cc2bfa3d7e181e3
Michael Silberstein <mikesilb123@gmail.com>
2018-03-07 13:31:42 -0500

    Initial tests for station class

--- Boston_MBTA/Station.Library.Tests/stationTests.cs
+++ Boston_MBTA/Station.Library.Tests/stationTests.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections.Generic;
+using Station.Library;
+using Train.Library;
 using Xunit;
 
 namespace Station.Library.Tests
@@ -9,10 +12,25 @@ namespace Station.Library.Tests
         public void IsDefinedProperly()
         {
             // Arrange
+            train F_train = new train("F Train");
+            train N_train = new train("N Train");
+            train R_train = new train("R Train");
 
-            // Act
+            List<train> KingsHighwayTrains = new List<train> { F_train };
+            List<train> ThirtyFourthStreetTrains = new List<train> { F_train, N_train, R_train };
+
+            station KingsHighway = new station("Kings Highway", KingsHighwayTrains);
+            station ThirtyFourthStreet = new station("34th Street", ThirtyFourthStreetTrains);
 
             // Assert
+            Assert.Equal(KingsHighway.GetName(), "Kings Highway");
+            Assert.Equal(ThirtyFourthStreet.GetName(), "34th Street");
+            Assert.Equal(KingsHighway.GetAssociatedTrains()[0], F_train);
+            Assert.Equal(KingsHighway.GetAssociatedTrains().Count, 1);
+            Assert.Equal(ThirtyFourthStreet.GetAssociatedTrains()[0], F_train);
+            Assert.Equal(ThirtyFourthStreet.GetAssociatedTrains()[1], N_train);
+            Assert.Equal(ThirtyFourthStreet.GetAssociatedTrains()[2], R_train);
+            Assert.Equal(ThirtyFourthStreet.GetAssociatedTrains().Count, 3);
         }
     }
 }
---
+++ Boston_MBTA/Station.Library/station.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Train.Library;
+
+namespace Station.Library
+{
+    public class station
+    {
+        private string _name;
+        private List<train> _associatedTrains;
+        public station(string name)
+        {
+            _name = name;
+        }
+        public station(string name, List<train> associatedTrains)
+        {
+            _name = name;
+            _associatedTrains = associatedTrains;
+        }
+        public string GetName()
+        {
+            return _name;
+        }
+        public void SetName(string name)
+        {
+            _name = name;
+        }
+        public List<train> GetAssociatedTrains()
+        {
+            return _associatedTrains;
+        }
+        public void SetStationList(List<train> associatedTrains)
+        {
+            _associatedTrains = associatedTrains;
+        }
+    }
+}
